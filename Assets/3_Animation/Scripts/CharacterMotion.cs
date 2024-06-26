0043a3fe49f3f8aec4cb4fa2ce7042ac171c8d37
김주환 <kimjh7204@gmail.com>
2024-06-26 17:51:57 +0900

    4방향 입력 처리

--- Assets/3_Animation/Scripts/CharacterMotion.cs
+++ Assets/3_Animation/Scripts/CharacterMotion.cs
@@ -1,19 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class CharacterMotion : MonoBehaviour
 {
     private Animator ani;
+    public Transform camPivot;
+
+    private Vector3 characterDir;
+
+    public float moveSpeed;
     
     void Start()
     {
         ani = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        camPivot.position = transform.position;
+        transform.Translate(characterDir * moveSpeed * Time.deltaTime);
+    }
+
     public void WalkForward(InputAction.CallbackContext context)
     {
-        ani.SetBool("isWalking", true);
+        if (context.started)
+        {
+            ani.SetBool("isWalking", true);
+        }
+
+        if (context.performed)
+        {
+            var dir = context.ReadValue<Vector2>();
+            var threeDDir = new Vector3(dir.x, 0f, dir.y);
+            characterDir = Vector3.Slerp(characterDir, threeDDir, 0.5f);
+        }
+
+        if (context.canceled)
+        {
+            ani.SetBool("isWalking", false);
+        }
     }
 }
